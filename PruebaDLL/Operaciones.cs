2c93d7fc3e2e644e28daf13b7fd7865f539371a3
Jose Roberto Perez Angulo <jperez@g-ventura.com>
2022-10-07 15:54:33 -0500

    Pruebas

---
+++ PruebaDLL/Operaciones.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaDLL
+{
+    public class Operaciones
+    {
+
+        //Funcion que realiza una accion
+        public static String Suma(int a, int b) {
+            int suma = a + b;
+            return "La sumatoria de los numero es: " + suma  ;
+        }
+
+
+
+    }
+}
